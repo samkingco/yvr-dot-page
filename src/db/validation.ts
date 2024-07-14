237a68f19a86cee487769f265e425a21b79a3c1b
Sam King <mail@samking.co>
2024-07-14 00:12:58 -0700

    Handle errors on submission form

--- src/db/validation.ts
+++ src/db/validation.ts
@@ -4,15 +4,15 @@ export const personSchema = v.object({
 	username: v.pipe(
 		v.string(),
 		v.trim(),
-		v.minLength(2, "must be at least 2 characters"),
-		v.maxLength(32, "must be 32 characters or less"),
+		v.minLength(2, "username must be at least 2 characters"),
+		v.maxLength(32, "username must be 32 characters or less"),
 	),
 	name: v.optional(
 		v.nullable(
 			v.pipe(
 				v.string(),
 				v.trim(),
-				v.maxLength(80, "must be 80 characters or less"),
+				v.maxLength(80, "name must be 80 characters or less"),
 			),
 		),
 		null,
@@ -20,17 +20,21 @@ export const personSchema = v.object({
 	does: v.pipe(
 		v.string(),
 		v.trim(),
-		v.maxLength(80, "must be 80 characters or less"),
+		v.minLength(1, "what you do is required"),
+		v.maxLength(80, "what you do must be 80 characters or less"),
 	),
 	website: v.pipe(
 		v.string(),
 		v.trim(),
-		v.maxLength(255, "must be 255 characters or less"),
+		v.url(),
+		v.minLength(1, "website is required"),
+		v.maxLength(255, "website must be 255 characters or less"),
 	),
 	bio: v.pipe(
 		v.string(),
 		v.trim(),
-		v.maxLength(240, "must be 240 characters or less"),
+		v.minLength(1, "bio is required"),
+		v.maxLength(240, "bio must be 240 characters or less"),
 	),
 })
 
